f1c24e35a2e9ee75786105619b15407a49380f58
dsvinfosolutions <vinay@dsvinfosolutions.com>
2020-01-06 13:44:34 +0530

    updated a user controller

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -118,7 +118,7 @@ class UserController extends Controller
             $user_details = Employee_detail::create($user_array);
             $msg = 'User successfully Added';
         }
-
+			/// updated a user controller
         /*if ($user->emp_id) {
             $user->employee_details()->update($user_array);
         }
